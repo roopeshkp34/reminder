da021a6848e88d5f78cac8d9f9b59419dd0f0868
roopeshkp34 <roopeshkp34@gmail.com>
2024-07-21 21:27:41 +0530

    :bug: STATIC_ROOT added

--- mail_reminder/settings.py
+++ mail_reminder/settings.py
@@ -126,6 +126,7 @@ STATIFILE_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
