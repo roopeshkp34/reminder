6d432719f6bc5ff8eb4aaac4ba2bd9854eb8fad3
roopeshkp34 <roopeshkp34@gmail.com>
2024-07-22 18:54:43 +0530

    :bug: Allowed host issue fixed

--- mail_reminder/settings.py
+++ mail_reminder/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["reminder-2f17.onrender.com"]
+ALLOWED_HOSTS = ["https://reminder-2f17.onrender.com"]
 
 
 # Application definition
