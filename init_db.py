aceea657eb97313a992ce2865f75b64daac121c7
Roopesh Kp <roopeshkp@Roopeshs-MacBook-Air.local>
2024-08-04 13:12:55 +0530

    :lipstick: removed unwanted prints

--- init_db.py
+++ init_db.py
@@ -8,5 +8,3 @@ from django.contrib.auth.models import User
 
 if (User.objects.filter(username= env["ADMIN_USERNAME"]).count()) == 0:
     User.objects.create_superuser(username= env["ADMIN_USERNAME"], password=env["ADMIN_PASSWORD"])
-
-print(f"User Exist")
